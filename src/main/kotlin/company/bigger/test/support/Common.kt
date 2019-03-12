00f50b50250ce6ebd4af7572fb118698232abd56
davidpodhola <david@podhola.net>
2019-03-12 21:45:38 +0100

    linting

--- src/main/kotlin/company/bigger/test/support/Common.kt
+++ src/main/kotlin/company/bigger/test/support/Common.kt
@@ -7,8 +7,8 @@ import java.util.Random
  */
 fun randomString(length: Int): String {
     fun ClosedRange<Char>.randomString(length: Int) =
-            (1..length)
-                    .map { (Random().nextInt(endInclusive.toInt() - start.toInt()) + start.toInt()).toChar() }
-                    .joinToString("")
+        (1..length)
+            .map { (Random().nextInt(endInclusive.toInt() - start.toInt()) + start.toInt()).toChar() }
+            .joinToString("")
     return ('a'..'z').randomString(length)
 }
